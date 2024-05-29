a4eca40ff75cef8b1fc5445dfb98c42d4181c644
Fahad <fahad.45854@iqra.edu.com>
2024-05-29 23:36:34 +0500

    Refactored the code

--- calculator.ts
+++ calculator.ts
@@ -1,9 +1,4 @@
-import {
-  addition,
-  dynamicCalculation,
-  multiplication,
-  substract,
-} from "./config.js";
+import { dynamicCalculation } from "./config.js";
 import { Prompts } from "./Prompt.js";
 
 let pr = await new Prompts(
@@ -12,12 +7,9 @@ let pr = await new Prompts(
   "Choose:\n1. Addition.\n2. Substract.\n3. Multiplication: "
 ).showPrompt();
 if (pr === "1") {
-  // addition();
   dynamicCalculation("+");
 } else if (pr === "2") {
-  // substract();
   dynamicCalculation("-");
 } else if (pr === "3") {
-  // multiplication();
   dynamicCalculation("*");
 }
